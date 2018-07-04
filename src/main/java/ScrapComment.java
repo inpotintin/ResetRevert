62101a2e9a8cd47325d90c8070d1841a88c6647e
Taewoong.yun <taewoong.yun@m2digital.co.kr>
2018-07-04 13:57:23 +0900

    #13 추가 수정 후 커밋

--- src/main/java/ScrapComment.java
+++ src/main/java/ScrapComment.java
@@ -8,6 +8,8 @@ public class ScrapComment {
 
         System.out.println("** 추가 수정1 **");
 
+        System.out.println("** 추가 수정2 **");
+
         System.out.println("** 댓글 아이디 수집 종료 **");
 
     }
