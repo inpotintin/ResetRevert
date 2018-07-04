cf220b1af457fd9f29e45d9c54f049ef4cf89550
Taewoong.yun <taewoong.yun@m2digital.co.kr>
2018-07-04 11:14:11 +0900

    #5 reset 후 #2 시점에서 잘못된 내용 수정 커밋

--- src/main/java/ScrapComment.java
+++ src/main/java/ScrapComment.java
@@ -4,8 +4,9 @@ public class ScrapComment {
 
         System.out.println("** 댓글 아이디 수집 시작 **");
 
-
         System.out.println("** 수정1 **");
+
+        System.out.println("** 수정2 **");
 
         System.out.println("** 댓글 아이디 수집 종료 **");
 
