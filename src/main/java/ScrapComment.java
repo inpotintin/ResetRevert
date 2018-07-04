2e6615e583133e77aabad7ab1f907de02714d4d3
Taewoong.yun <taewoong.yun@m2digital.co.kr>
2018-07-04 14:01:07 +0900

    Revert "#14 추가 수정 후 커밋"
    This reverts commit bed599da21f161a6decde3ad6521a3dfa9a0e5d4.

--- src/main/java/ScrapComment.java
+++ src/main/java/ScrapComment.java
@@ -10,8 +10,6 @@ public class ScrapComment {
 
         System.out.println("** 추가 수정2 **");
 
-        System.out.println("** 추가 수정3 **");
-
         System.out.println("** 댓글 아이디 수집 종료 **");
 
     }
