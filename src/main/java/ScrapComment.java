32b88f4670ba99db216e80dfbb093289333f6e29
Taewoong.yun <taewoong.yun@m2digital.co.kr>
2018-07-04 14:18:28 +0900

    #18 최초 코드

--- src/main/java/ScrapComment.java
+++ src/main/java/ScrapComment.java
@@ -4,10 +4,6 @@ public class ScrapComment {
 
         System.out.println("** 댓글 아이디 수집 시작 **");
 
-        System.out.println("** 수정1 **");
-
-        System.out.println("** 추가 수정 1 **");
-
         System.out.println("** 댓글 아이디 수집 종료 **");
 
     }
