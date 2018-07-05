93761428b21197a080b6fb369a73514e1fe852d9
Taewoong.yun <taewoong.yun@m2digital.co.kr>
2018-07-05 11:28:30 +0900

    merge test 3

--- src/main/java/ScrapComment.java
+++ src/main/java/ScrapComment.java
@@ -10,6 +10,8 @@ public class ScrapComment {
 
         System.out.println("** git add 2 **");
 
+        System.out.println("** git add 3 **");
+
         System.out.println("** 댓글 아이디 수집 종료 **");
 
     }
