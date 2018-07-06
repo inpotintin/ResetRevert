ec20e1803e529bec734eef21c09d2912bd3047d2
Taewoong.yun <taewoong.yun@m2digital.co.kr>
2018-07-06 10:25:59 +0900

    Revert "feature 4"
    This reverts commit 6cd27f2c328f68c2cf133da247fb870f699dbe7a.

--- src/main/java/ScrapComment.java
+++ src/main/java/ScrapComment.java
@@ -22,8 +22,6 @@ public class ScrapComment {
 
         System.out.println("** feature 3 **");
 
-        System.out.println("** feature 4 **");
-
         System.out.println("** 댓글 아이디 수집 종료 **");
 
     }
