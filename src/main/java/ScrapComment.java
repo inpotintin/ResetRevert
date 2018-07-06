bff4c0f4f37b2144bfd026ff9afa0ebfb35696d3
Taewoong.yun <taewoong.yun@m2digital.co.kr>
2018-07-06 11:35:40 +0900

    feature 4

--- src/main/java/ScrapComment.java
+++ src/main/java/ScrapComment.java
@@ -22,6 +22,8 @@ public class ScrapComment {
 
         System.out.println("** feature 3 **");
 
+        System.out.println("** feature 4 **");
+
         System.out.println("** 댓글 아이디 수집 종료 **");
 
     }
