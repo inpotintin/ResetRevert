c117b4f5660726916254cf7ca34dfe6d52c5f6af
Taewoong.yun <taewoong.yun@m2digital.co.kr>
2018-07-09 11:20:05 +0900

    Revert "feature 9"
    This reverts commit a07787f2a89e07553a3bd1a96ce0e7b587e45c06.

--- src/main/java/ScrapComment.java
+++ src/main/java/ScrapComment.java
@@ -32,8 +32,6 @@ public class ScrapComment {
 
         System.out.println("** feature 8 **");
 
-        System.out.println("** feature 9 **");
-
         System.out.println("** 댓글 아이디 수집 종료 **");
 
     }
