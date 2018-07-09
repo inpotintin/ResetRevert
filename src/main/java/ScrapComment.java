3103151d32fd23dc463d331da4e557ce144d0d98
Taewoong.yun <taewoong.yun@m2digital.co.kr>
2018-07-09 16:23:14 +0900

    Revert "feature 3 cherry-pick"
    This reverts commit 38cae0fda4e2a1aa0ea73e4be95c582d656a3069.

--- src/main/java/ScrapComment.java
+++ src/main/java/ScrapComment.java
@@ -4,12 +4,6 @@ public class ScrapComment {
 
         System.out.println("** 댓글 아이디 수집 시작 **");
 
-        System.out.println("** feature 1 **");
-
-        System.out.println("** feature 2 **");
-
-        System.out.println("** feature 3 **");
-
         System.out.println("** 댓글 아이디 수집 종료 **");
 
     }
