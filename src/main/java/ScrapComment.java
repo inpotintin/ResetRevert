2fc39a343c2fe3133ca1a735899f688b84e94de0
Taewoong.yun <taewoong.yun@m2digital.co.kr>
2018-07-09 15:23:16 +0900

    init

--- src/main/java/ScrapComment.java
+++ src/main/java/ScrapComment.java
@@ -4,36 +4,6 @@ public class ScrapComment {
 
         System.out.println("** 댓글 아이디 수집 시작 **");
 
-        System.out.println("** 수정1 **");
-
-        System.out.println("** git add 1 **");
-
-        System.out.println("** git add 2 **");
-
-        System.out.println("** git add 3 **");
-
-        System.out.println("** git stash 1 **");
-
-        System.out.println("** git stash 2 **");
-
-        System.out.println("** feature 1 **");
-
-        System.out.println("** feature 2 **");
-
-        System.out.println("** feature 3 **");
-
-        System.out.println("** feature 4 **");
-
-        System.out.println("** feature 5 **");
-
-        System.out.println("** feature 6 **");
-
-        System.out.println("** feature 7 **");
-
-        System.out.println("** feature 8 **");
-
-        System.out.println("** feature 9 **");
-
         System.out.println("** 댓글 아이디 수집 종료 **");
 
     }
