84864d469cb68166d2dcff3571806bb56dc055c0
Taewoong.yun <taewoong.yun@m2digital.co.kr>
2018-07-09 18:42:35 +0900

    Revert "2 files commit 1"
    This reverts commit ce6bd7287204ae184054c0a75042de758265dc15.

--- src/main/java/ScrapCommentWorker.java
+++ src/main/java/ScrapCommentWorker.java
@@ -4,8 +4,6 @@ public class ScrapCommentWorker {
 
         System.out.println("** 댓글 등록 유저 수집 시작 **");
 
-        System.out.println("** worker 1 **");
-
         System.out.println("** 댓글 등록 유저 수집 종료 **");
 
     }
