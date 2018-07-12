b091b3246d97fcf73c33ae5ba79ab8b8ce1e7cec
Taewoong.yun <taewoong.yun@m2digital.co.kr>
2018-07-12 11:18:39 +0900

    cherry pick 3

--- src/main/java/ScrapComment.java
+++ src/main/java/ScrapComment.java
@@ -8,6 +8,8 @@ public class ScrapComment {
 
         System.out.println("** cherry pick 2 **");
 
+        System.out.println("** cherry pick 3 **");
+
         System.out.println("** 댓글 아이디 수집 종료 **");
 
     }
