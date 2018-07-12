8aab4682c8c387f16a1846e0a5c2b6ae07ad407d
Taewoong.yun <taewoong.yun@m2digital.co.kr>
2018-07-12 10:36:31 +0900

    init

--- src/main/java/ScrapComment.java
+++ src/main/java/ScrapComment.java
@@ -4,24 +4,6 @@ public class ScrapComment {
 
         System.out.println("** 댓글 아이디 수집 시작 **");
 
-        System.out.println("** feature 1 **");
-
-        System.out.println("** feature 2 **");
-
-        System.out.println("** feature 3 **");
-
-        System.out.println("** feature 4 **");
-
-        System.out.println("** feature 5 **");
-
-        System.out.println("** feature 6 **");
-
-        System.out.println("** feature 7 **");
-
-        System.out.println("** feature 8 **");
-
-        System.out.println("** reset **");
-
         System.out.println("** 댓글 아이디 수집 종료 **");
 
     }
