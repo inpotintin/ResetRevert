5e4b0a296fbb61fd0273c200b550c9eaa288aa8d
Taewoong.yun <taewoong.yun@m2digital.co.kr>
2018-07-12 11:23:50 +0900

    Revert "cherry pick 2"
    This reverts commit 57b82d93cbe9e8a57d58a19dbdc0dfdf23a55c60.

--- src/main/java/ScrapComment.java
+++ src/main/java/ScrapComment.java
@@ -6,8 +6,6 @@ public class ScrapComment {
 
         System.out.println("** cherry pick 1 **");
 
-        System.out.println("** cherry pick 2 **");
-
         System.out.println("** 댓글 아이디 수집 종료 **");
 
     }
