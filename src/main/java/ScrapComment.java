33596179197b65e8ab3cd8c2db021653f178e89e
Taewoong.yun <taewoong.yun@m2digital.co.kr>
2018-07-16 18:47:39 +0900

    rebase test init

--- src/main/java/ScrapComment.java
+++ src/main/java/ScrapComment.java
@@ -10,12 +10,6 @@ public class ScrapComment {
 
         System.out.println("** cherry pick 3 **");
 
-        System.out.println("** rebase 1 **");
-
-        System.out.println("** rebase 2 **");
-
-        System.out.println("** rebase 3 **");
-
         System.out.println("** 댓글 아이디 수집 종료 **");
 
     }
