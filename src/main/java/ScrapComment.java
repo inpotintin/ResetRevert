3feade5726f2d24315488c0a876eaceb23a2b330
Taewoong.yun <taewoong.yun@m2digital.co.kr>
2018-07-16 18:45:30 +0900

    rebase commit 1

--- src/main/java/ScrapComment.java
+++ src/main/java/ScrapComment.java
@@ -10,16 +10,6 @@ public class ScrapComment {
 
         System.out.println("** cherry pick 3 **");
 
-        System.out.println("** amend test 1 **");
-
-        System.out.println("** amend test 2 **");
-
-        System.out.println("** amend test 3 **");
-
-        System.out.println("** amend test 4 **");
-
-        System.out.println("** amend test 5 **");
-
         System.out.println("** rebase 1 **");
 
         System.out.println("** 댓글 아이디 수집 종료 **");
