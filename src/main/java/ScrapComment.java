f085d3649d628ee0b569bb3021d96684b7e23520
Taewoong.yun <taewoong.yun@m2digital.co.kr>
2018-07-16 18:45:51 +0900

    rebase commit 2

--- src/main/java/ScrapComment.java
+++ src/main/java/ScrapComment.java
@@ -12,6 +12,8 @@ public class ScrapComment {
 
         System.out.println("** rebase 1 **");
 
+        System.out.println("** rebase 2 **");
+
         System.out.println("** 댓글 아이디 수집 종료 **");
 
     }
