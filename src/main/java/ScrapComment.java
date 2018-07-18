0fed2705838bbf401d78a1741e60611a7b98f271
Taewoong.yun <taewoong.yun@m2digital.co.kr>
2018-07-18 13:57:21 +0900

    client 1

--- src/main/java/ScrapComment.java
+++ src/main/java/ScrapComment.java
@@ -14,6 +14,8 @@ public class ScrapComment {
 
         System.out.println("** master 2 **");
 
+        System.out.println("** client 1 **");
+
         System.out.println("** 댓글 아이디 수집 종료 **");
 
     }
