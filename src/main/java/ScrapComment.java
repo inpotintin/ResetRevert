121c22777840e1b2489fa2b40ff274f931af884e
Taewoong.yun <taewoong.yun@m2digital.co.kr>
2018-07-18 13:54:41 +0900

    master 2

--- src/main/java/ScrapComment.java
+++ src/main/java/ScrapComment.java
@@ -12,6 +12,8 @@ public class ScrapComment {
 
         System.out.println("** master 1 **");
 
+        System.out.println("** master 2 **");
+
         System.out.println("** 댓글 아이디 수집 종료 **");
 
     }
